45d9d93b298829a315461df433f9ce34b5763e27
meskedal <mariuesk@stud.ntnu.no>
2019-06-03 23:44:17 -0700

    [Refactor] Changed Script name for Objects

---
+++ Assets/obstacleBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacleBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject resetButton;
+    private resetButtonHandler resetHandler;
+    void Start()
+    {
+        resetButton = GameObject.Find("ResetButton");
+        resetHandler = (resetButtonHandler) resetButton.GetComponent(typeof(resetButtonHandler));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hei");
+        resetHandler.resetBall();
+    }
+}
