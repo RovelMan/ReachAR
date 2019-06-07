d681c19ed1517402964532b94117ddc19dbff613
RovelMan <jotdeepgahla@hotmail.com>
2019-06-06 21:17:37 -0700

    updated stage 3

---
+++ Assets/Scripts/fixed_wall1_upper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class fixed_wall1_upper : MonoBehaviour
+{
+    float lockPos = 0;
+    
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(lockPos, GameObject.Find("Wall1").transform.rotation.eulerAngles.y, lockPos);
+        try {
+            GameObject.Find("Wall1").transform.position = new Vector3(
+                GameObject.Find("Wall1").transform.position.x,
+                GameObject.Find("Board").transform.position.y+9,
+                GameObject.Find("Wall1").transform.position.z
+            );        }
+        catch (Exception e) {
+            print("Error");
+        }
+    }
+}
